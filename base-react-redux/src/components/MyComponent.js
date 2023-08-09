9df16fdab1a02433010e50a6bb5c898497332cc1
kuroh <fantasiaferver@gmail.com>
2023-08-09 09:19:23 +0700

    bai 17

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -2,9 +2,16 @@ import React from "react";
 
 class MyComponent extends React.Component {
     //JSX
+    state = {
+        firtsName: 'Chinh',
+        age: 22,
+        sex: 'male'
+    }
     render() {
         return (
-            <div>My first component</div>
+            <div>My name is: {this.state.firtsName}
+                my age is : {this.state.age}
+            </div>
         )
     }
 }
