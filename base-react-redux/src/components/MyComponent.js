67f22455bab01f4bc65e1c23cf1ed6737f164df6
kuroh <fantasiaferver@gmail.com>
2023-08-09 10:18:04 +0700

    bai 26

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -3,21 +3,32 @@ import React from "react";
 class MyComponent extends React.Component {
     //JSX
     state = {
-        firtsName: 'Chinh',
+        firstName: 'Chinh',
         age: 22,
         sex: 'male'
     }
     handleClick(event) {
-        console.log(event)
+        // console.log(event)
+        console.log(this.state.firstName + ' ' + this.state.age)
+        // console.log("random", Math.floor((Math.random() * 100) + 1))
+
+        this.setState({
+            firstName: 'Pham Van Chinh',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+        // this.setState({
+        //     age: Math.floor((Math.random() * 100) + 1)
+        // })
     }
     handleMouseOver(event) {
         console.log(event)
+
     }
     render() {
         return (
-            <div>My name is: {this.state.firtsName}
+            <div>My name is {this.state.firstName} and my age is {this.state.age}
                 <button onMouseOver={this.handleMouseOver}>Hover me</button>
-                <button onClick={this.handleClick}>Click me</button>
+                <button onClick={(event) => this.handleClick(event)}>Click me</button>
             </div>
         )
     }
