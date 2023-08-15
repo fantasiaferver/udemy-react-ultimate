6a595c2fed5575b9c54cb3e018a2834ce2f36e57
kuroh <fantasiaferver@gmail.com>
2023-08-15 11:24:59 +0700

    start todo-app

--- base-react-redux/src/App.js
+++ base-react-redux/src/App.js
@@ -5,15 +5,15 @@ import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>Hello world Pham Chinh
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>Hello world Pham Chinh
+//         <MyComponent></MyComponent>
+//       </div>
+//     )
+//   }
+// }
 // const App = () => {
 //   return (
 //     <div>Hello world
@@ -34,23 +34,14 @@ class App extends React.Component {
 //   );
 // }
 
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
+const App = () => {
 
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world Chinh Pham
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
+  return (
+    <div className="app-container">
+      Hello
+      <button className='btn btn-primary'>Button</button>
+    </div>
+  );
+}
 
 export default App;
